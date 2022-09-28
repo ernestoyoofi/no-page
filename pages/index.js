cfac7e4b98b9ae905968e2b317139c2ec7e827ba
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-09-28 18:47:11 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-function Home() {
+function Home(x) {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,7 +15,7 @@ function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        {JSON.stringify(x)}
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
